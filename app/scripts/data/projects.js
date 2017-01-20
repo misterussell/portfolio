6356a41c25d0ac92f89e91c9f77ece3c6abb1125
Maxwell Russell <maxruss.87@gmail.com>
2017-01-19 18:02:58 -0600

    updating spelling errors

--- app/scripts/data/projects.js
+++ app/scripts/data/projects.js
@@ -12,7 +12,7 @@ export default [
     tools: ['React', 'Backbone', 'SASS', 'jService API'],
     image: '../assets/images/jeopardy-fullscreen.png',
     about: 'Utilizing an API that compiled years of Jeopardy questions and answers, this project was an early introduction into integrating a large-scale API with React.',
-    challenge: 'The data provided by the jService API was not restructed when the API was created. This meant that the data-sets available were not always similarly structured. The main challenge with this project was figuring out how to search through API endpoints for data that could be rendered in similar ways, without having to manipulate the data to fit my needs. I tackled this by utilizing recursive algorithms that only returned endpoints that fit my desired criteria. Once this was completed I concentrated on an energetic design that blended bold fonts and bold colours.',
+    challenge: 'The data provided by the jService API was not restructured when the API was created. This meant that the data-sets available were not always similarly structured. The main challenge with this project was figuring out how to search through API endpoints for data that could be rendered in similar ways, without having to manipulate the data to fit my needs. I tackled this by utilizing recursive algorithms that only returned endpoints that fit my desired criteria. Once this was completed I concentrated on an energetic design that blended bold fonts and bold colors.',
     background: '../assets/backgrounds/wov.png'
   }
 ];
